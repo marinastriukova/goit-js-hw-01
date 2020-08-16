85df539305a016179a13d79c5145a9e6ce9fbacb
Marina Striukova <marinastriukova@gmail.com>
2020-08-16 17:50:05 +0300

    добавляет скрипты

---
+++ js/task-4.js
@@ -0,0 +1,35 @@
+const orderPieces = prompt('Выберите кол-во товара');
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+let totalPrice = pricePerDroid * orderPieces; // Write code on this line
+let balanceCredit = credits - totalPrice; // Write code on this line
+let message;
+
+// Write code under this line
+
+if(orderPieces === null){
+    message = CANCELED_BY_USER;
+} else if(totalPrice > credits){
+    message = ACCESS_DENIED;
+} else if(totalPrice <= credits){
+    message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`
+}
+    
+
+console.log(message);
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
